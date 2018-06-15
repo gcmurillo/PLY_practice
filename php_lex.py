f9d76086254759b03a9bf30385355ba28fb0ad03
gcmurillo <gcmurillo@outlook.com>
2018-06-14 19:42:35 -0500

    reformat code

--- php_lex.py
+++ php_lex.py
@@ -40,21 +40,14 @@ tokens = [
 ]
 
 reserved = {
-    'abstract': 'ABSTRACT',
-    'and': "AND",
-	'array': "ARRAY",
-	'as': "AS",
+	'and': "AND",
 	'break': "BREAK", 
-	'callable': "CALLABLE", 
 	'case': "CASE", 
 	'catch' : "CATCH", 
-	'class' : "CLASS", 
 	'clone' : "CLONE", 
 	'const' : "CONST", 
 	'continue' : "CONTINUE", 
-	'declare' : "DECLARE", 
-	'default' : "DEFAULT", 
-	'die' : "DIE", 
+	'default' : "DEFAULT",
 	'do' : "DO", 
 	'echo' : "ECHO", 
 	'else' : "ELSE", 
@@ -65,87 +58,68 @@ reserved = {
 	'endforeach' : "ENDFOREACH", 
 	'endif' : "ENDIF", 
 	'endswitch' : "ENDSWITCH", 
-	'endwhile' : "ENDWHILE", 
-	'eval' : "EVAL", 
-	'exit' : "EXIT", 
-	'extends' : "EXTENDS", 
+	'endwhile' : "ENDWHILE",
+	'exit' : "EXIT",  
 	'final' : "FINAL", 
 	'for' : "FOR", 
-	'foreach' : "FOREACH", 
-	'function' : "FUNCTION", 
+	'foreach' : "FOREACH",  
 	'global' : "GLOBAL", 
 	'goto' : "GOTO", 
-	'if' : "IF", 
-	'implements' : "IMPLEMENTS", 
-	'include' : "INCLUDE", 
-	'include_once' : "INCLUDE ONCE", 
-	'instanceof' : "INSTANCEOF",
-	'insteadof' : "INSTEADOF",
-	'interface' : "INTERFACE",
-	'isset' : "ISSET",
-	'list' : "LIST",
-	'namespace' : "NAMESPACE",
+	'if' : "IF",
 	'new' : "NEW",
 	'or' : "OR",
 	'print' : "PRINT",
-	'private' : "PRIVATE",
-	'protected' : "PROTECTED",
-	'public' : "PUBLIC",
-	'require' : "REQUIRE",
-	'require_once' : "REQUIRE ONCE",
-	'return' : "RETURN", 
-	'static' : "STATIC", 
-	'switch' : "SWITCH", 
-	'throw' : "THROW", 
-	'trait' : "TRAIT", 
-	'try' : "TRY", 
-	'unset' : "UNSET", 
-	'use' : "USE", 
-	'var' : "VAR", 
+	'return' : "RETURN",
+	'switch' : "SWITCH",
+	'try' : "TRY",
 	'while' : "WHILE", 
 	'xor' : "XOR"
 }
 
-def Milexer():
-	
-	t_SUMA    = r'\+'
-	t_RESTA   = r'-'
-	t_DIVISION  = r'/'
-	t_MULTIPLICACION = r'\*'
-	t_PARENTESIS_IZQ  = r'\('
-	t_PARENTESIS_DER  = r'\)'
-	t_LLAVE_DER = r'\}'
-    t_LLAVE_IZQ = r'\{'
-    t_COMENTARIO = r'\\\\'
-    t_CORCHETE_DER = r'\]'
-    t_CORCHETE_IZQ = r'\['
-    t_ASIGNACION = r'='
-    t_COMILLA_DOBLE = r'"'
-    t_MAYOR_QUE = r'>'
-    t_MENOR_QUE = r'<'
-    t_MENOR_IGUAL_QUE = r'<='
-    t_MAYOR_IGUAL_QUE = r'>='
-    t_OP_AND = r'&&'
-    t_OP_OR = r'\|\|'
-    t_IGUAL = r'=='
-    t_IDENTICO = r'==='
-    t_DIFERENTE = r'!='
-    t_DIFERENT = r'<>'
-    t_NO_IDENTICO = r'!=='
-    t_NAVE_ESPACIAL = r'<=>'
-    t_FUSION_NULL = r'\?\?'
-    t_INCREMENTO = r'\+\+'
-    t_DECREMENTO = r'--'
-    t_NOT = r'!'
-    t_PUNTO_COMA = r';'
-    t_DOS_PUNTOS = r':'
-    t_PUNTO = r'\.'
-    t_ignore  = ' \t'
-	def t_ID(t):
+
+# Regular expression rules for simple tokens
+t_SUMA    = r'\+'
+t_RESTA   = r'-'
+t_DIVISION  = r'/'
+t_MULTIPLICACION = r'\*'
+t_PARENTESIS_IZQ  = r'\('
+t_PARENTESIS_DER  = r'\)'
+t_LLAVE_DER = r'\}'
+t_LLAVE_IZQ = r'\{'
+t_COMENTARIO = r'\\\\'
+t_CORCHETE_DER = r'\]'
+t_CORCHETE_IZQ = r'\['
+t_ASIGNACION = r'='
+t_COMILLA_DOBLE = r'"'
+t_MAYOR_QUE = r'>'
+t_MENOR_QUE = r'<'
+t_MENOR_IGUAL_QUE = r'<='
+t_MAYOR_IGUAL_QUE = r'>='
+t_OP_AND = r'&&'
+t_OP_OR = r'\|\|'
+t_IGUAL = r'=='
+t_IDENTICO = r'==='
+t_DIFERENTE = r'!='
+t_DIFERENT = r'<>'
+t_NO_IDENTICO = r'!=='
+t_NAVE_ESPACIAL = r'<=>'
+t_FUSION_NULL = r'\?\?'
+t_INCREMENTO = r'\+\+'
+t_DECREMENTO = r'--'
+t_NOT = r'!'
+t_PUNTO_COMA = r';'
+t_DOS_PUNTOS = r':'
+t_PUNTO = r'\.'
+t_ignore  = ' \t'
+
+
+def t_ID(t):
 	    r'^\$[a-zA-Z]*'
 	    t.type = reserved.get(t.value, 'ID')
 	    return t
-	def t_NUMERO(t):
+
+
+def t_NUMERO(t):
 		r'\d+'
 		t.value = int(t.value)    
     	return t
